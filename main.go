91683c08320a345c888d9230b8520eb2113e030f
Yao Long <yaolongt@gmail.com>
2025-04-19 00:21:15 +0800

    fix: removed unused code

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"product/configs"
-	_ "product/docs"
 	"product/grpc"
 	"product/storage"
-	// "product/routes"
-	// "backend/mq"
 )
 
 func main() {
-	configs.InitEnv() // init env
-	// mq.Init()           // init rabbitmq connection
+	configs.InitEnv()            // init env
 	storage.GetStorageInstance() // init db
 	grpc.Init()
 }
